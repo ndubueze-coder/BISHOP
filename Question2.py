3d9b58da288aba50ff18c8015ed22a46b892690d
ndubueze-coder <samuelndubueze865@yahoo.com>
2025-03-13 07:00:15 -0700

    this is my first commit

---
+++ Question2.py
@@ -0,0 +1,315 @@
+# #myname = input("enter your name here: " )
+# #print(f"Good Morning {myname}, how are you doing?: ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # STRING MANUPLATION
+
+# # MY_VAR ="The sun is yello"
+# # print(MY_VAR.upper())
+
+# # email= "JHONDOE@GMAIL.COM"
+# # print(email)
+# # print(email.lower)
+
+# # name = "           mejeh emmanuel chimeremeze"
+
+# # print(f"Hell0, {name.title().strip()}")
+
+# # setence = "president buhari is nigeria's president"
+# # print(sentece.replace("buhari","tinibu"))
+
+
+# # state ="abia state, nigeria, africa"
+# # print(state.split(",")[0])
+
+# # my_str = "aba is a beatuful place"
+# # """indexing in python is carried out on array like datatype example are string and list """
+
+# #  )my_str = "123456789"
+# #   print(len(my_str))
+# #   print(my_str[-6:-1]
+
+# # name = input("enter your name(fullname):")
+# # age =int(input("enter your age:"))
+# # email =input("enter your email address:")
+# # stateoforigin = input("enter your state of origin:")
+
+# # print(f"hello{name}, you were born in {2025 - age}, emailaddress is {email}, {stateoforigin} is your birth place")
+
+
+
+
+
+
+
+# # my_code = 'hello'
+# # my_code = input('enter your string:')
+# new_code =[]
+
+# # for i in range(len(my_code)):# iterate over indices
+# # if i % 2 == 0:# check if index is 
+# # new_code.append(my_code[i].upper()) else:
+# # new_code.append(my_code[i]..lower())
+
+# my_list=["apple", "upper", "banana", "cherry", "child", "six", "over"]
+# # new_list = list((2, 56,34))
+# #print(new_list)(3, 44 ,34)
+
+# #print(new_list)
+# # print(my_list[0:5:2])
+# # new_list=list(my_list[0:3:2])
+# # print(new_list)
+# # print(my_list[0:3:2])
+
+# # my_list=["5","6", "5", "6", "5", "3","over" ]
+# # print(my_list[0:5:8])
+
+
+# # first_list=["name", "age," "email"]
+# # second_list["john",13, john@email.com]
+
+# # mydict = dict(zip(first_list, second_list))
+# # print(mydict)
+
+# #Arithmetic operators
+# # print(3 + 8)#arithmetic
+# # print(10 - 9)#subtration
+# # print(10 * 4)#multiplication
+# # print(10 | 5)#division
+# # print(2 ** 4)
+
+
+
+
+# #comparism operators
+# # x=5
+# # v=10
+
+# # print(x ==y)
+# # print(x >=  y)
+# # print(x <= y)
+# # print(x != y)
+# # print(x > y)
+
+# # Logical Operators
+# # x and y
+# # x or y 
+# # x not y 
+
+# # #condictional Statement  
+# # name = input("enter, name: ") 
+# # program = input("enter, your program(techrise\stream B):")
+# # if "techrise" == program:
+# #   print("hello", name)
+# # elif "stream B" == program:
+# #     print("hello", name)
+# # else:
+# #     print(f"we not recoganize {name} in this class")
+
+
+
+
+# # binary= 7 & 5
+
+# #print(7 & 5)
+
+
+# #print(3 << 2)
+
+# #print(100 + 3)
+
+# # print(5 + 4 - 7 + 4)
+
+# #if statement
+# # b = int(input('enter your number:' ))
+# # a = int(input('enter your number:' ))
+# # if a > b:
+# #   print('b is greater than a')
+# # elif a < b:
+# #   print("b is greater than a")
+
+
+
+# #binary = number
+
+# #class applicant:
+# def_int_("self, name, age, gender")
+# self.name= name
+# self.age = age
+# self.gender = gender
+
+# def assign_department(self):
+#     if self.age < 18 or self.age > 50:
+#         return "not eligible"
+
+#         if self.gender == "male":
+#             if self.age < 25:
+#                 return "custumer care"
+#             elif self.age <45:
+#                 return "engerring"
+#         else:
+#          return "security"
+#     else: return "admin"
+
+
+# name = input('enter, your, name:')
+# age = int(input('enter, your, age:'))
+# gender = input('enter, your, gender:')
+
+# if age < 18 or age > 50:
+#  print(f'{name} you are not qualified for this recruitment')
+# else:
+#  if (gender == 'male' and age < 25) or (gender == 'female' and age < 31):
+#     print('f{name} you will be in the customer care department')
+#  elif gender == 'male' and age <= 45:
+#     print(f'{name} you are place in the enginerrin departmen')
+#  elif gender =='female' and age >= 31:
+#     print(f'{name} you are place in the admin department')
+#  else:
+#         print(f'{name} you are place in the security department')
+
+# i = 1
+# while i < 6:
+#     print(i)
+#     if i ==3:
+#         break
+#     i += 1
+
+# i = 0
+# while i < 6:
+#     i += 1
+#     if i == 3:
+#         continue
+#     print(i)
+
+# fruits = ["apple," "banana," "cherry"]
+# for x in fruits:
+#     print(x)
+
+# fruits = ["apple", "banana," "cherry"]
+# for x in fruits:
+#     if x == "banana":
+#      continue
+# print(x)
+
+# adj = ["red", "big," "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+
+# for x in adj:
+#     for y in fruits:
+#         print(x, y)
+
+
+# print("#")
+# print("#")
+# print("#")
+# print("#")
+
+# range(4):for i in 
+#     for j in range(4):
+#         print("#", end = "  ")
+#     print()
+
+
+# ndubueze = ("apple," "cynthia," "amaka," "gold")
+# print("apple," "cynthia," "amaka," "gold")
+# print(ndubueze)
+# print(ndubueze)
+
+
+# def print_multiplication_table():
+#     for i in range(2,13):
+#         print(f"mutiplication table of {i}:")
+#         for j in range(1,11):
+#             print(f"{i} x {j} = {i * j}") 
+#         print()
+
+# print_multiplication_table()
+
+
+# def print_calender_range():
+#     year = int(input("enter the year: "))
+#     start_month_input = ("enter the starting month (name or nuber):")
+#     end_month_ = input("enter the ending month (name or number):")
+
+# #convert start month input to interger if it's a number
+# if start_month_input.isdigit():
+#     start_month = int(start_month_input)
+# else:
+#     start_month = list(calender.month_name).index (start_month_input.capitaise)
+
+#     # convert end month input to integer if it's a number
+#     if end_month_input.isdigit():
+#         end_month = int(end_month_input)
+#     end_month = list(calender.month_name).index(end_month_input.capitalise:())
+# else:
+#     #  print the calender for each month in the range
+#     print(
+#         f"\nthe calender from {calender.month_name[start_month]} to {calender.month_name}{end_month}"
+#     )
+# for month in range(start_month, end_month + 1):
+#     print("\n", calender.month_name[month], year)
+#     print(calender. monthcalender(year,month))
+#     print("\n", calender.month(year, month))
+
+#     print_calender_range()
+
+# for i in range(5):
+#     for j in range(i + 1):
+#         print("#", end = " ")
+#     print() 
+
+
+######################inverted right tringle#######################
+# n = 7
+# for i in range(n,0,-1):
+#     for j in range(n-i):
+#         print('--k', end='')
+#     for k in range(i):
+#         print('#  ', end= '')
+#     print()
+
+# n = 7
+# for i in range(n):
+#     for j in range(n-1):
+#         print("  ", end = "")
+#     for k in range(i):
+#         print(" * ", end ="")
+#     print()
+# n = 1
+# for i in range(n, 0, -i):
+#     for j in range(n-i):
+#         print(" ", end = "")
+#     for k in range(i):
+#         print(" * ", end ="")
+#     print()
+
+# def exponmentim (num, power):
+    #   resualt = num "" power
+    #   return result
+
+    # print(exponmential(10,2))
+# def multiply(num1, num2, num3):
+#     result  * num2
+#     x = [*num3]
+#     for i in x:
+#         result = result * i
+#     returun 
+
+
+
+
+
+
